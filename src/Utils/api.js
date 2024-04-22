044dfae1643a08884b02c9252abd81316e7812c7
Marcus441 <psminchia4153@hotmail.com>
2024-04-22 12:34:37 +1000

    made authenticate and register async

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -1,43 +1,39 @@
 
-const authenticate = (credentials) => {
+const authenticate = async (credentials) => {
     const URL = 'http://4.237.58.241:3000/user/login';
-    return fetch(URL, {
+    const response = await fetch(URL, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(credentials),
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            if (data.token) {
-                localStorage.setItem('token', data.token);
-                return true;
-            }
-            return false;
-        });
+    });
+    const data = await response.json();
+    console.log(data);
+    if (data.token) {
+        localStorage.setItem('token', data.token);
+        return true;
+    }
+    return false;
 };
 
-const register = (credentials) => {
+const register = async (credentials) => {
     const URL = 'http://4.237.58.241:3000/user/register';
 
-    return fetch(URL, {
+    const response = await fetch(URL, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(credentials),
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            if (data.token) {
-                localStorage.setItem('token', data.token);
-                return true;
-            }
-            return false;
-        });
+    });
+    const data = await response.json();
+    console.log(data);
+    if (data.token) {
+        localStorage.setItem('token', data.token);
+        return true;
+    }
+    return false;
 };
 
 export { authenticate, register };
