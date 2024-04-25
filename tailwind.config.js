339d4566b3e21a363c309d380d64272d3107eedb
Marcus441 <psminchia4153@hotmail.com>
2024-04-25 19:50:39 +1000

    implemented tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'], // adjust this to match the file extensions you are using
+  darkMode: 'media', // or remove this line entirely
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
