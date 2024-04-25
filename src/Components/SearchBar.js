54b249defef5ca5ae0a15296498887346792c27b
Marcus441 <psminchia4153@hotmail.com>
2024-04-25 19:42:20 +1000

    created algorithm for search suggestions

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
+import useFetch from '../Hooks/useFetch';
 
 const SearchBar = ({ onSearch }) => {
     const [searchTerm, setSearchTerm] = useState('');
+    const { data: countries = [] } = useFetch('http://4.237.58.241:3000/countries') || {};
 
     const handleInputChange = (event) => {
         setSearchTerm(event.target.value);
     };
-    
+
     const handleKeyPress = (event) => {
         if (event.key === 'Enter') {
             handleSearch();
@@ -18,6 +20,12 @@ const SearchBar = ({ onSearch }) => {
         console.log('Searching for:', searchTerm);
     };
 
+    const suggestions = searchTerm
+        ? countries.filter(country =>
+            country.toLowerCase().startsWith(searchTerm.toLowerCase())
+        )
+        : [];
+
     return (
         <div className='search-bar'>
             <input
@@ -27,7 +35,16 @@ const SearchBar = ({ onSearch }) => {
                 onChange={handleInputChange}
                 onKeyDown={handleKeyPress}
             />
-            <button  onClick={handleSearch}>Search</button>
+            <button onClick={handleSearch}>Search</button>
+            {suggestions.length > 0 && (
+                <div className="suggestions">
+                    {suggestions.map((suggestion, index) => (
+                        <div key={index} onClick={() => setSearchTerm(suggestion)}>
+                            {suggestion}
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
